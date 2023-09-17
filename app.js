3fd9b63c742fd0e38022b92dd2c94616d8027c42
nlewis84 <nathan@differential.com>
2023-09-17 14:54:46 -0500

    chore: fleshed out some possible endpoints

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// Import required modules
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-
 const { insertGreekText } = require('./greekTextService');
 
+// Initialize the Express app
+const app = express();
+
+// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// API endpoint to insert Greek text
+// API endpoint to insert Greek text into the database
+// It expects a JSON body with a 'filePath' key
 app.post('/insert', async (req, res) => {
   try {
     const { filePath } = req.body;
@@ -17,7 +21,31 @@ app.post('/insert', async (req, res) => {
   }
 });
 
-// Start the Express server
+// API endpoint to fetch Greek text by book, chapter, and verse
+// It expects these as query parameters
+app.get('/greekText', async (req, res) => {
+  const { book, chapter, verse } = req.query;
+  // TODO: Fetch the Greek text from the database
+  // TODO: Return the fetched text as JSON
+});
+
+// API endpoint to fetch vector data for a list of words
+// It expects a JSON body with a 'words' key containing an array of words
+app.post('/wordVectors', async (req, res) => {
+  const { words } = req.body;
+  // TODO: Fetch vector data for these words from the database
+  // TODO: Return the fetched data as JSON
+});
+
+// API endpoint to fetch other occurrences of a specific word in scriptures
+// It expects the word as a query parameter
+app.get('/otherOccurrences', async (req, res) => {
+  const { word } = req.query;
+  // TODO: Fetch other occurrences of the word from the database
+  // TODO: Return the fetched data as JSON
+});
+
+// Start the Express server on port 3000 or the port defined in the environment
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
